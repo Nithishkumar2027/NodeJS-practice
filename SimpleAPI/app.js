e5022ec6a2b4e735c6bdd9506976516b7fb448bc
Nithishkumar2027 <mnnithishkumar20@gmail.com>
2020-10-11 02:22:36 +0530

    Static folder set to Public

--- SimpleAPI/app.js
+++ SimpleAPI/app.js
@@ -16,6 +16,9 @@ app.set('view engine', 'handlebars')
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+// Setting static folder
+app.use(express.static('public'))
+
 // Homepage route
 app.get('/', (req, res) =>{
     res.render('index', {
