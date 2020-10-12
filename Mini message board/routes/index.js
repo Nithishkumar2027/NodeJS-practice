cbee97034efb302c08f3078f78d101399c5ff30c
Nithishkumar2027 <mnnithishkumar20@gmail.com>
2020-10-12 23:58:46 +0530

    Message handlers

--- Mini message board/routes/index.js
+++ Mini message board/routes/index.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const moment = require('moment')
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Kumar",
+    added: 'October 12th 2020, 11:21:35 pm'
+  },
+  {
+    text: "Hello world",
+    user: "Ijaan",
+    added: 'October 12th 2020, 12:21:35 am'
+  }
+]
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
+
+router.get('/create-message', (req, res) => {
+  res.render('createMessage', {title: 'Create Message'})
+})
+
+
+router.post('/create-message', (req, res)=>{
+  messages.push(handleMessage(req.body))
+  res.redirect('/')
+})
+
+const handleMessage = (messageRequest) => {
+  return {
+    text: messageRequest.message,
+    user: messageRequest.username,
+    added: moment().format('MMMM Do YYYY, h:mm:ss a')
+  }  
+}
 
 module.exports = router;
