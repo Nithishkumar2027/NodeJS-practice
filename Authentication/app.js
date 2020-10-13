d6de3481e1fe1710dd53dc4bba212e0c981c9db1
Nithishkumar2027 <mnnithishkumar20@gmail.com>
2020-10-13 23:48:41 +0530

    Setting up localstrategy and serializations

--- Authentication/app.js
+++ Authentication/app.js
@@ -30,6 +30,34 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }))
+
+passport.use(
+    new LocalStrategy((username, password, done) => {
+      User.findOne({ username: username }, (err, user) => {
+        if (err) { 
+          return done(err);
+        };
+        if (!user) {
+          return done(null, false, { msg: "Incorrect username" });
+        }
+        if (user.password !== password) {
+          return done(null, false, { msg: "Incorrect password" });
+        }
+        return done(null, user);
+      });
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user)
+    })
+})
+
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(express.urlencoded({extended: false}))
