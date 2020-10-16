6754255ae2f26786d87430a46d0e89ae2a18e7f7
Nithish kumar <mnnithishkumar20@gmail.com>
2020-10-16 21:04:08 +0530

    Created JWT token

--- JWT/app.js
+++ JWT/app.js
@@ -6,9 +6,32 @@ const port = process.env.PORT || 3000
 const app = express()
 
 app.use(morgan('dev'))
+
+// Routes
 app.get('/api', (req, res) => {
     res.json({
         message: 'Welcome to the API 🌏'
+    })
+})
+
+app.post('/api/posts', (req, res) => {
+    res.json({
+        message: 'Post created 🤗'
+    })
+})
+
+app.post('/api/login', (req, res) => {
+    // Mock user
+    const user = {
+        id: 1,
+        username: 'Kumar',
+        email: 'kumar@gmail.com'
+    }
+
+    jwt.sign({user: user}, 'secretkey', (err, token) =>{
+        res.json({
+            token: token
+        })
     })
 })
 
