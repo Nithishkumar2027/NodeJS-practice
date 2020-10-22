668e6941d8fb99b135b4722ebc687e2f03b9b3fe
Nithish kumar <mnnithishkumar20@gmail.com>
2020-10-22 23:07:24 +0530

    Without Promise chaining

--- MongoDB/promises-eg.js
+++ MongoDB/promises-eg.js
@@ -10,3 +10,26 @@ doWorkPromise.then((result) => {
 }).catch((error) => {
     console.log('Error: ', error)
 })
+
+// Add function
+const add = (a,b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000);
+    })
+}
+
+// Without Promise chaining
+add(1,2).then((sum) => {
+    console.log('Without Promise chaining')
+    console.log('Sum: ',sum)
+    add(sum, 4).then((sum2) => {
+        console.log('Sum 2: ',sum2)
+    }).catch((err) => {
+        console.log(err)
+    })
+}).catch((err) => {
+    console.log(err)
+})
+
