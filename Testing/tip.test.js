98855cc52da3083382d27e6fdab52f4f067bcbe1
Nithish kumar <mnnithishkumar20@gmail.com>
2020-10-31 18:04:15 +0530

    Normal testing

---
+++ Testing/tip.test.js
@@ -0,0 +1,10 @@
+const { calculateTip } = require('./tip')
+
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10, .3)
+    if (total !== 13) {
+        throw new Error('Total tip should be 13. But got ', total)
+    }
+})
+
+
