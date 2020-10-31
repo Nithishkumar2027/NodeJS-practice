1538d9e3b79975d99a33e34bfc4d814a1f24ead7
Nithish kumar <mnnithishkumar20@gmail.com>
2020-10-31 18:42:22 +0530

    Using expect

--- Testing/tip.test.js
+++ Testing/tip.test.js
@@ -1,10 +1,18 @@
 const { calculateTip } = require('./tip')
 
+// Using conditions
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3)
     if (total !== 13) {
         throw new Error('Total tip should be 13. But got ', total)
     }
 })
+
+// Using expect
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10, .5)
+    expect(total).toBe(15)
+})
+
 
 
