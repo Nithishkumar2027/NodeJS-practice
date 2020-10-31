e3834f9511229f6494cdb18d0cda6d4fecff54cb
Nithish kumar <mnnithishkumar20@gmail.com>
2020-10-31 21:07:09 +0530

    Async/await testing

--- Testing/math.test.js
+++ Testing/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('./math')
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require('./math')
 
 // Using conditions
 test('Should calculate total with tip', () => {
@@ -33,5 +33,18 @@ test('Async test demo', (done) => {
     setTimeout(() => {
         expect(2).toBe(2)
         done()
-    }, 2000);
+    }, 1000);
+})
+
+test('Should add two numbers', (done) => {
+    add(2, 3).then((sum) => {
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+// Async/await
+test('Should add two numbers async/await', async () => {
+    const sum = await add(5, 2)
+    expect(sum).toBe(7)
 })
